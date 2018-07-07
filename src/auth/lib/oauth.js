4affb54a704f2db239c6f4fd449dd601511f3ac6
Khoa Huynh <khoawin92@gmail.com>
2018-07-07 11:49:50 -0700

    changed oauth to linkedin

--- src/auth/lib/oauth.js
+++ src/auth/lib/oauth.js
@@ -9,27 +9,28 @@ const authorize = req => {
   let code = req.query.code;
   console.log('1. the code: ', code);
 
-  return superagent.post('https://www.googleapis.com/oauth2/v4/token')
+  return superagent.post('https://www.linkedin.com/oauth/v2/accessToken')
     .type('form')
     .send({
       code: code,
-      client_id: process.env.GOOGLE_CLIENT_ID,
-      client_secret: process.env.GOOGLE_CLIENT_SECRET,
-      redirect_uri: `${process.env.API_URL}/oauth/google/code`,
+      client_id: process.env.LINKEDIN_CLIENT_ID,
+      client_secret: process.env.LINKEDIN_CLIENT_SECRET,
+      redirect_uri: `${process.env.API_URL}/oauth`,
       grant_type: 'authorization_code',
     })
-    .then( response => {
-      let googleToken = response.body.access_token;
-      console.log('2. google token is: ', googleToken);
-      return googleToken;
-    })
-    .then(token => {
-      return superagent.get('https://www.googleapis.com/plus/v1/people/me/openIdConnect')
-        .set('Authorization', `Bearer ${token}`)
+    .then(response => {
+      let linkedInToken = response.body.access_token;
+      console.log('2. linkedin token is: ', linkedInToken);
+      return superagent.get('https://api.linkedin.com/v1/people/~:(formatted-name,first-name,last-name,email-address,picture-url)')
+        .set('Authorization', `Bearer ${linkedInToken}`)
         .then(response => {
-          let googleUser = response.body;
-          console.log('3. Google User', googleUser);
-          return googleUser;
+          let linkedInUser = {
+            name: response.text.split('<formatted-name>')[1].split('<')[0],
+            email: response.text.split('<email-address>')[1].split('<')[0],
+            pictre: response.text.split('<picture-url>')[1].split('<')[0],
+          };
+          console.log('3. linkedInUser User', linkedInUser);
+          return linkedInUser;
         });
     })
     .then(user => {
@@ -42,4 +43,4 @@ const authorize = req => {
     });
 };
 
-export default {authorize};
+export default { authorize };
